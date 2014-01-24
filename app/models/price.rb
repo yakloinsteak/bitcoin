5c59d24040783c106e51e9a32d9af6ab71ea02ee
Evgeny Rahman <evgeny.rahman@staples.com>
2014-01-24 15:01:42 -0500

    Adding fetch price method

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,6 +1,11 @@
 class Price < ActiveRecord::Base
 
-def self.by_symbol_and_date(symbol, reqdate)
-  price = Price.find_by symbol: symbol, reqdate: reqdate
-  price = Price.fetch_price(symbol, reqdate) if price.empty?
+  def self.by_symbol_and_date(symbol, reqdate)
+    price = Price.find_by symbol: symbol, reqdate: reqdate
+    price = Price.fetch_price(symbol, reqdate) if price.empty?
+  end
+
+  def self.fetch_price(symbol, reqdate)
+    0
+  end
 end
